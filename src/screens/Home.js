92cba0ef5f92408c4d118df312b35f5493d59eee
Dusan Peric <pericdusan6@gmail.com>
2026-03-23 20:38:13 +0100

    modul 20

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,70 @@
 import React from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import { Text, View, StyleSheet, Button, Image } from "react-native";
+import Swiper from "react-native-swiper";
 
 const Home = ({ navigation }) => (
-  <View>
-    <Text>Welcome to Home Screen</Text>
-    <Button title="Go to About" onPress={() => navigation.navigate("About")} />
-    <Button title="Open Drawer" onPress={() => navigation.openDrawer()} />
+  <View style={styles.container}>
+    <View style={styles.sliderContainer}>
+      <Swiper
+        autoplay
+        autoplayTimeout={5}
+        activeDotColor="#22D4FF"
+        showsButtons={true}
+        onIndexChanged={(index) => {
+          console.log("Promijenio se indeks na ", index);
+        }}
+      >
+        <View style={styles.item}>
+          <Image
+            source={require("../../assets/banner-1.jpg")}
+            style={styles.imgItem}
+            resizeMode="cover"
+          />
+        </View>
+        <View style={styles.item}>
+          <Image
+            source={require("../../assets/banner-2.jpg")}
+            style={styles.imgItem}
+            resizeMode="cover"
+          />
+        </View>
+        <View style={styles.item}>
+          <Image
+            source={require("../../assets/banner-3.jpg")}
+            style={styles.imgItem}
+            resizeMode="cover"
+          />
+        </View>
+      </Swiper>
+    </View>
   </View>
 );
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+
+  sliderContainer: {
+    width: "90%",
+    height: 200,
+    justifyContent: "center",
+    alignSelf: "center",
+    marginTop: 10,
+    borderRadius: 8,
+  },
+
+  item: {
+    flex: 1,
+    justifyContent: "center",
+  },
+
+  imgItem: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 8,
+  },
+});
 
 export default Home;
